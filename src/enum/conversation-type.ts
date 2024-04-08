0d6842aa6db9a6a38273551f1aa154bbbfd4a385
nimamleo <nimamahini81@gmail.com>
2024-04-08 03:29:06 -0700

    devops

--- src/enum/conversation-type.ts
+++ src/enum/conversation-type.ts
@@ -1,31 +1,4 @@
-import { ConversationType as PrismaConversationType } from '@prisma/client';
-
 export enum ConversationType {
   DIRECT = 'direct',
   GROUP = 'group',
-}
-
-export function fromAppConversationType(
-  value: ConversationType,
-): PrismaConversationType {
-  switch (value) {
-    case ConversationType.DIRECT:
-      return PrismaConversationType.Direct;
-    case ConversationType.GROUP:
-      return PrismaConversationType.Group;
-    default:
-      return null;
-  }
-}
-export function toAppConversationType(
-  value: PrismaConversationType,
-): ConversationType {
-  switch (value) {
-    case PrismaConversationType.Group:
-      return ConversationType.GROUP;
-    case PrismaConversationType.Direct:
-      return ConversationType.DIRECT;
-    default:
-      return null;
-  }
 }
