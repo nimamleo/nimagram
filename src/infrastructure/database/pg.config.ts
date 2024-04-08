5e16c86164be7d81644d08e8567d73b6be75a8c5
nimamleo <nimamahini81@gmail.com>
2024-04-08 06:55:03 -0700

    devops

--- src/infrastructure/database/pg.config.ts
+++ src/infrastructure/database/pg.config.ts
@@ -17,26 +17,26 @@ export const postgresConfig = registerAsWithLog<
   ConfigFactory<IPgConfig>
 >(POSTGRES_CONFIG, () => {
   if (!process.env.PG_PORT) {
-    throw Error('PG_PORT not provided');
+    throw Error('POSTGRES_PORT not provided');
   }
-  if (!process.env.PG_HOST) {
-    throw Error('PG_HOST not provided');
+  if (!process.env.POSTGRES_HOST) {
+    throw Error('POSTGRES_HOST not provided');
   }
-  if (!process.env.PG_NAME) {
-    throw Error('PG_NAME not provided');
+  if (!process.env.POSTGRES_DB) {
+    throw Error('POSTGRES_DB not provided');
   }
-  if (!process.env.PG_USERNAME) {
-    throw Error('PG_USERNAME not provided');
+  if (!process.env.POSTGRES_USERNAME) {
+    throw Error('POSTGRES_USERNAME not provided');
   }
-  if (!process.env.PG_PASSWORD) {
+  if (!process.env.POSTGRES_PASSWORD) {
     throw Error('PG_PASSWORD not provided');
   }
 
   return {
-    port: +process.env.PG_PORT,
-    host: process.env.PG_HOST,
-    db_name: process.env.PG_NAME,
-    username: process.env.PG_USERNAME,
-    password: process.env.PG_PASSWORD,
+    port: +process.env.POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST,
+    db_name: process.env.POSTGRES_NAME,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
   };
 });
